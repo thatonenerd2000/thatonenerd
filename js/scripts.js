4c15932629b7aa3306ff42c55eb4e27389f47cd1
Shadman Chowdhury <shadmanchowdhury@Shadmans-MBP.home>
2018-07-22 19:36:51 -0400

    3.5/4 done

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,13 @@ $(document).ready(function(){
     mobile:true,
     once:true
   });
+  var userFeed = new Instafeed({
+      get: 'user',
+      userId: '5678013860',
+      accessToken: '5678013860.1677ed0.74cfd557ff1b4fd8bc8e81211eb5f25e',
+      resolution: 'standard_resolution',
+      limit: 12,
+      template: "<div style='padding-bottom:10px;' class='col-sm-4'><a data-lightbox='jpg' href='{{image}}' title='{{caption}}'><img style='height:250px;' src='{{image}}' alt='{{caption}}' class='img-fluid'></a></div>"
+  });
+  userFeed.run();
 })
