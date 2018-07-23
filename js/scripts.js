64a4d04e490243fd49f57d54444e8e31888fb2a0
Shadman Chowdhury <shadmanchowdhury@Shadmans-MBP.home>
2018-07-22 20:52:45 -0400

    done

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
       accessToken: '5678013860.1677ed0.74cfd557ff1b4fd8bc8e81211eb5f25e',
       resolution: 'standard_resolution',
       limit: 12,
-      template: "<div style='padding-bottom:10px;' class='col-sm-4'><a data-lightbox='jpg' href='{{image}}' title='{{caption}}'><img style='height:250px;' src='{{image}}' alt='{{caption}}' class='img-fluid'></a></div>"
+      template: "<div data-adaptive-background data-ab-css-background style='padding-bottom:10px;' class='col-sm-3'><a data-lightbox='jpg' href='{{image}}' title='{{caption}}'><img  style='height:250px;' src='{{image}}' alt='{{caption}}' class='img-fluid'></a></div>"
   });
   userFeed.run();
 })
